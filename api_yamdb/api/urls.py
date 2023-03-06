8fc887c75eec240a2938e6ca860cb5db35e82b9b
last-ui <slastukhin_av@mail.ru>
2023-03-06 15:45:36 +0700

    fixed isort errors

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -2,8 +2,8 @@ from django.urls import include, path
 from rest_framework import routers
 
 from api.views import (APIUser, CategoryViewSet, CommentViewSet, GenreViewSet,
-                       get_token, ReviewViewSet, send_code, TitleViewSet,
-                       UserViewSet)
+                       ReviewViewSet, TitleViewSet, UserViewSet, get_token,
+                       send_code)
 
 app_name = 'api'
 
